ae3482ce07579d57a93c15132d2f17979d8e525c
Abhi <abs_sh@hotmail.co.uk>
2026-05-25 15:52:11 +0100

    add testimonials data file

---
+++ testimonials.js
@@ -0,0 +1,52 @@
+const TESTIMONIALS = [
+  {
+    quote: "The best PM I've ever worked with. His technical background makes him really know what he's doing.",
+    author: "Mert Duzgun",
+    role: "Engineer · Builder.ai"
+  },
+  {
+    quote: "There is always one person steering the ship and that, my friend, is you.",
+    author: "Nathan Ziabek",
+    role: "Engineer · Builder.ai"
+  },
+  {
+    quote: "You are the most talented technical project manager I have ever met. I have been working for 15 years in this industry.",
+    author: "Jerry Thimothy J",
+    role: "Colleague"
+  },
+  {
+    quote: "You know a lot about what's done here, have a great attitude and get actual shit done.",
+    author: "Pedro Dias",
+    role: "Colleague · Builder.ai"
+  },
+  {
+    quote: "Forward thinking and driven to deliver. Always looking to optimise and support operational roadmaps. A great asset to any company.",
+    author: "Michelle Oliveira Dos Santos",
+    role: "Head of Sales Ops Digital Transformation"
+  },
+  {
+    quote: "I admire your work ethic. You find fulfilment in your work, which is something you should cherish.",
+    author: "Istvan Hoka",
+    role: "Colleague · Builder.ai"
+  },
+  {
+    quote: "Best managerial skills you have. I had gone through lot of managers in my career. Never seen a person like you.",
+    author: "Dharani Arun",
+    role: "Colleague"
+  },
+  {
+    quote: "Thank you for being a great PM.",
+    author: "Raphael Damasceno",
+    role: "Engineer · Builder.ai"
+  },
+  {
+    quote: "I have seen him develop from a conversational analyst to a Product Owner with ease and great expertise. The projects I have worked on with him have all been extremely successful.",
+    author: "Jaspreet Dhaliwal",
+    role: "Reporting Lead (Global OTT) · Sky"
+  },
+  {
+    quote: "Really great to see the progress you've made and the comfort level you have now across the Bx org!",
+    author: "Rohan Patel",
+    role: "Colleague · Builder.ai"
+  },
+];
